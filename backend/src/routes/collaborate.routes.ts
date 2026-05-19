72fe3c4e007376e7b4568c16af1a5b430cf3ff1f
Suraj-3285 <suraj.mnnit85@gmail.com>
2026-05-19 10:21:14 +0530

    added collaborator controller and route

---
+++ backend/src/routes/collaborate.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { inviteCollaborator,getCollaborator,updateCollaboratorRole,removeCollaborator,getMyCollaborations, } from "../controllers/collaborate.controller";
+import { protect } from "../middlewares/auth.middleware";
+const router = Router({ mergeParams: true});
+
+router.get("/my/collaborations", protect, getMyCollaborations);
+router.get("/:storyId/collaborators",protect,getCollaborator);
+router.post("/:storyId/collaborators",protect,inviteCollaborator);
+router.put("/:storyId/collaborators/:collaboratorId",protect,updateCollaboratorRole);
+router.delete("/:storyId/collaborators/:collaboratorId",protect,removeCollaborator);
+
+export default router;
